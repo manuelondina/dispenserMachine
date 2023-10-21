d9768c0b6de3c9b001016c0137e5d7ae0be99d23
Manuel Rafael Ondina Aja <manuel-rafael.ondina-aja@hp.com>
2023-10-21 23:27:46 +0200

    MOA: Remove Note to add in readme.md

--- dispenser/src/main/java/com/sdworks/dispenser/app/service/BuyDrinkService.java
+++ dispenser/src/main/java/com/sdworks/dispenser/app/service/BuyDrinkService.java
@@ -4,7 +4,6 @@ import com.sdworks.dispenser.core.DispenserMachine;
 import com.sdworks.dispenser.core.Drink;
 
 public class BuyDrinkService {
-
     public static void buyDrink(DispenserMachine dispenserMachine, String drinkName) {
         Drink drink = dispenserMachine.getDrinks().stream()
                 .filter(d -> d.getName().equals(drinkName)).findFirst()
@@ -22,11 +21,4 @@ public class BuyDrinkService {
             throw new IllegalArgumentException("Invalid drink");
         }
     }
-    // XXX:
-    // Please note that I did not implement the case to ask the user if he
-    // wants to buy another drink. If not then he would receive the change, in this
-    // case user
-    // will only receive remaining coins if he cancels the order. Reason is because
-    // in the requirements it says that we don't want any interactive keyboard
-    // input.
 }
